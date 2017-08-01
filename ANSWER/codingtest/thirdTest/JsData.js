7f844b31bce893da985fbe1d450b7e969af96bf7
abhbtbb <abhbtbb1@gmail.com>
2017-08-01 23:27:41 +0900

    third Test : JSON 데이터를 가공하여 차트로 구현하기 진행중
    1. JSON 데이터를 가공해서 유의미한 정보로 만든다.
    2. 해당 정보를 SVG차트로 표현한다.
    
    현재 JSON데이터를 가공하는데 까지 진행함

---
+++ ANSWER/codingtest/thirdTest/JsData.js
@@ -0,0 +1,72 @@
+
+	// 1. 데이터를 받아온다.
+	// 2. 데이터를 가공한다.
+	// 3. 렌더링으로 보낸다.
+	
+	// surveyData		==> gender[남성, 여성], age[20, 30, 40, 50], questions[1번문항, 2번문항, 3번문항, 4번문항, 5번문항]
+	// surveyActionData ==> 
+	
+	//{"answer": ["d", "b", "a", "a", "c"], "gender": "female", "age": 30}
+	
+	
+	// 순수 데이터 처리로직
+	class JsData {
+		constructor(surveyData,surveyActionData) {
+			this._rawData = surveyActionData;
+			this._subjects = this.makeSubject(surveyData);
+			this._checkRules = [];
+			this._result = {};
+		}
+		
+		makeSubject(surveyData) {
+			let arr = [];
+			for(let one in surveyData) {
+				if(!(surveyData.hasOwnProperty(one))) continue;
+				arr.push(one);
+			}
+			arr.splice(arr.indexOf("title"),1); 			// title은 날린다.
+			return arr;
+		}
+		
+		// 체크 알고리즘을 추가한다.
+		addCheckRule(checkRule) {
+			this._checkRules.push(checkRule);
+		}
+		
+		// 저장된 체크 알고리즘을 순회한다.
+		runCheckRules(oneData, targetRules) {
+			let tempResult = [];
+			let temp;
+			
+			for(let i = 0; i < targetRules.length; i++) {
+				temp = this._checkRules[i](oneData, targetRules[i])
+				if(!temp) return false;
+				tempResult.push(temp);
+			}
+			return tempResult;
+		}
+		
+		// 가공된 데이터의 각 항목 갯수를 카운트 한다.
+		enterResultNum(data) {		
+			let i = 0, len = data.length-1, temp = this.result['result'];
+			for(i; i < len; i++) {
+				temp[data[i]] = temp[data[i]] || {};
+				temp = temp[data[i]];
+			}
+			temp[data[i]] = temp[data[i]] ? temp[data[i]] + 1 : 1;
+		}
+		
+		// 해당 데이터를 가져와서 targetRules에 따라서 데이터 가공 하여 갯수를 카운트 한뒤 반환하게 한다.
+		getData(targetRules) {
+			let len = this._rawData.length, i = 0, temp;
+			this.result = {}; // 초기화
+            this.result['targetRules'] = targetRules;
+            this.result['result'] = {};
+
+			for(i; i < len; i++) {
+				temp = this.runCheckRules(this._rawData[i], targetRules);
+				if(temp) this.enterResultNum(temp);
+			}
+			return this.result;
+		}
+	}
